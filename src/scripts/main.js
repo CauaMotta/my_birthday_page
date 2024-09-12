ec2751a5db8dec07445ef56da96449e8e6429c95
Cauã Motta <ocauamotta@gmail.com>
2024-09-12 10:59:08 -0300

    reajuste

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,25 +22,50 @@ document.addEventListener('DOMContentLoaded', () => {
         document.getElementById('counter').innerHTML = `${daysToEvent}d ${hoursToEvent}h ${minutesToEvent}m ${secondsToEvent}s`;
     }, 1000)
 
+    const iGo = document.getElementsByClassName('btn')[0];
+    iGo.addEventListener('click', () => {
+        clearInterval(counter);
+
+        document.getElementsByClassName('box')[0].remove();
+        document.getElementsByClassName('gift')[0].remove();
+        document.getElementsByTagName('img')[0].setAttribute('src', './assets/joia.png');
+        document.getElementsByClassName('title')[0].innerText = 'Valeu, tmj!';
+    })
+
+    const inHold = document.getElementsByClassName('btn')[1];
+    inHold.addEventListener('click', () => {
+        clearInterval(counter);
+
+        document.getElementsByClassName('birthday')[0].appendChild(document.getElementsByClassName('text')[0]);
+
+        document.getElementsByClassName('box')[0].remove();
+        document.getElementsByClassName('gift')[0].remove();
+        document.getElementsByTagName('img')[0].setAttribute('src', './assets/obras.jpeg');
+        document.getElementsByClassName('title')[0].innerText = 'Página em Construção...';
+        document.getElementsByClassName('text')[0].innerText = '...Por favor aguarde';
+    })
+
     const yesBtn = document.getElementsByClassName('yes')[0];
     yesBtn.addEventListener('click', () => {
         clearInterval(counter);
 
+        document.getElementsByClassName('box')[0].remove();
+        document.getElementsByClassName('gift')[0].remove();
         document.getElementsByTagName('img')[0].setAttribute('src', './assets/joia.png');
         document.getElementsByClassName('title')[0].innerText = 'Valeu, tmj!';
-        document.getElementsByClassName('title')[1].remove();
-        document.getElementsByClassName('gift')[0].remove();
     })
 
     const noBtn = document.getElementsByClassName('no')[0];
     noBtn.addEventListener('click', () => {
 
+        document.getElementsByClassName('birthday')[0].appendChild(document.getElementsByClassName('text')[0]);
         document.getElementsByClassName('birthday')[0].appendChild(document.getElementById('counter'));
         document.getElementsByClassName('birthday')[0].setAttribute('class', 'birthday alert');
 
+        document.getElementsByClassName('gift')[0].remove();
+        document.getElementsByClassName('box')[0].remove();
         document.getElementsByTagName('img')[0].setAttribute('src', './assets/gatinho-brabo.avif');
         document.getElementsByClassName('title')[0].innerText = 'E ta esperando o que?';
-        document.getElementsByClassName('title')[1].innerText = 'SEU PRAZO ESTA ACABANDO!';
-        document.getElementsByClassName('gift')[0].remove();
+        document.getElementsByClassName('text')[0].innerText = 'SEU PRAZO ESTA ACABANDO!';
     })
 })
